6aef1e6b7fbbdc2e39318493b2afade618991661
Andrei Kardovich <akardovich@gmail.com>
2022-07-11 05:20:23 +0300

    log in to the system

--- auth/users/urls.py
+++ auth/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginVeiw
 
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
+    path('login', LoginVeiw.as_view()),
 ]
