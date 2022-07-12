8ed1ed6d476164fe320df51cb03008f381a99315
Andrei Kardovich <akardovich@gmail.com>
2022-07-12 20:58:14 +0300

    logout

--- auth/users/urls.py
+++ auth/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginVeiw, UserView
+from .views import RegisterView, LoginVeiw, UserView, Logout
 
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginVeiw.as_view()),
     path('user', UserView.as_view()),
+    path('logout', Logout.as_view()),
 ]
 
